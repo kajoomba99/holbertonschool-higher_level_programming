d624967aa199d7746148d7a3f012aeacdff6c5af
Steven Brand <steven.brand1999@gmail.com>
2020-05-05 17:34:26 -0500

    Correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if not sentence:
         return (len(sentence), None)
-    return (len(sentence), sentence[0])
+    else:
+        return (len(sentence), sentence[0])
