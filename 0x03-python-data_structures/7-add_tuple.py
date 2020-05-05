60ef8a4c69d5196db640a6c0f30ccd11f5ab39e9
Steven Brand <steven.brand1999@gmail.com>
2020-05-05 17:29:46 -0500

    Corrections

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) == 1:
-        fv = tuple_a[0] + tuple_b[0]
-        sv = tuple_a[1]
-    if len(tuple_b) == 2:
-        fv = tuple_a[0] + tuple_b[0]
-        sv = tuple_a[1] + tuple_b[1]
-    if len(tuple_b) == 0:
+    if len(tuple_a) >= 1:
         fv = tuple_a[0]
+    else:
+        fv = 0
+    if len(tuple_b) >= 1:
+        fv += tuple_b[0]
+    else:
+        fv += 0
+    if len(tuple_a) >= 2:
         sv = tuple_a[1]
+    else:
+        sv = 0
+    if len(tuple_b) >= 2:
+        sv += tuple_b[1]
+    else:
+        sv += 0
     return (fv, sv)
