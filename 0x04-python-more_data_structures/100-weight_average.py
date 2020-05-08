fcf96399a595917480ec9328d325864b254ba18b
Steven Brand <steven.brand1999@gmail.com>
2020-05-07 19:56:15 -0500

    Update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     num = 0
     div = 0
     if my_list == []:
-	    return 0
+        return 0
     for e in my_list:
         num += e[0] * e[1]
         div += e[1]
