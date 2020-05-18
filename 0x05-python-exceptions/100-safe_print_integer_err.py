3649274bcc4fcf9a0a76fd36fed7eaf8410feac7
Steven Brand <steven.brand1999@gmail.com>
2020-05-18 15:17:47 -0500

    stderr message correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
-        print(err, file=sys.stderr)
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
     else:
         return True
