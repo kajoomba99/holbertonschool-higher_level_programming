03c1f7df6e9ee937f1f8f1fcafe28d81c3aa552f
Steven Brand <steven.brand1999@gmail.com>
2020-05-19 18:18:57 -0500

    Se me olvido la 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+0. My first square:
+Write an empty class Square that defines a square:
+You are not allowed to import any module
+"""
+
+
+class Square:
+
+    """This is an empty class"""
+    pass
