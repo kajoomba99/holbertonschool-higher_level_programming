1f33e8b3a57010438bf41adcac9d71a53ea8fc32
Steven Brand <steven.brand1999@gmail.com>
2020-05-19 19:28:08 -0500

    Update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,24 +10,8 @@ You are not allowed to import any module
 class Square:
     """Square class"""
     def __init__(self, size=0, position=(0, 0)):
-        """Entry point"""
-        self.__size = size
-        self.__position = position
-
-    def area(self):
-        """Returns the current square area"""
-        return self.__size ** 2
-
-    def my_print(self):
-        """print a square of #"""
-        if self.__size == 0:
-            print("")
-        else:
-            for i in range(self.__position[1]):
-                print("")
-            for i in range(self.__size):
-                print(" " * self.__position[0], end='')
-                print("#" * self.__size)
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -51,10 +35,25 @@ class Square:
     @position.setter
     def position(self, value):
         """set value to position"""
-        if not isinstance(value, tuple) or value != 2:
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
         if not isinstance(value[0], int) or value[0] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         if not isinstance(value[1], int) or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """print a square of #"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__position[1]):
+                print("")
+            for i in range(self.__size):
+                print(" " * self.__position[0], end='')
+                print("#" * self.__size)
