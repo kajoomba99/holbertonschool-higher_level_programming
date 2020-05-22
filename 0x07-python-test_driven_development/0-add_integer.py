a803a289b83af41288c6ae5fea00b5dbd53bcaed
Steven Brand <steven.brand1999@gmail.com>
2020-05-22 15:53:53 -0500

    TDD proyect

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Module with function
+That
+Returns an integer: the addition of a and b
+"""
+
+
+def add_integer(a, b=98):
+
+    """
+    Return a + b
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return a + b
