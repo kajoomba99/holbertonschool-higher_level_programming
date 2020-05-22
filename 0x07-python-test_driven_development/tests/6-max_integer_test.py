046aea153a3423dc8a56ab09fc4288e1a10859da
Steven Brand <steven.brand1999@gmail.com>
2020-05-22 16:10:50 -0500

    Add comments to TestMaxInteger Class

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,30 +1,51 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""
+Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+
+    """
+    Class of unittest
+    """
+
     def test_variousFunction(self):
+        """
+        Test normal cases
+        """
         self.assertEqual(max_integer([1, 2]), 2)
         self.assertEqual(max_integer([-5, 2, 5]), 5)
         self.assertEqual(max_integer([-5, -2, -3]), -2)
         self.assertEqual(max_integer([1, 1, 1]), 1)
 
     def test_Error(self):
+        """
+        Test error cases
+        """
         with self.assertRaises(TypeError):
             max_integer(True)
 
     def test_Error_2(self):
+        """
+        Test error cases
+        """
         with self.assertRaises(TypeError):
             max_integer(4, 2, 1, 2)
 
     def test_Error_3(self):
+        """
+        Test error cases
+        """
         with self.assertRaises(TypeError):
             max_integer("Melkin", 1)
 
     def test_Error_4(self):
+        """
+        Test error cases
+        """
         with self.assertRaises(TypeError):
             max_integer("", "")
 
