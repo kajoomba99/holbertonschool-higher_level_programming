1eeec2ad2967fa221c09d88a40acea86a66f48c9
kajoomba99 <steven.brand1999gmail.com>
2020-05-25 19:24:50 -0500

    PyCodeStyle corrections

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,16 +1,36 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
+"""This module defines the class Rectangle"""
+
+
 class Rectangle:
+    """Class that builds a rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        """
+        Initializes instance of the rectangle
+        Args:
+            width: width for __width attribute
+            height: height for __height atributte
+        """
+        self.height = height
+        self.width = width
 
     @property
     def height(self):
+        """
+        Getter function of height
+        Returns:
+            The height of the rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Setter function of height
+        Args:
+            value: value for __height attribute
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -19,10 +39,20 @@ class Rectangle:
 
     @property
     def width(self):
+        """
+        Getter function of width
+        Returns:
+            The width of the rectangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Setter function of width
+        Args:
+            value: value for __width attribute
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
