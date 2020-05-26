6c644c2cef3c928d4216e1e4c5720437d025518e
kajoomba99 <steven.brand1999gmail.com>
2020-05-25 19:02:58 -0500

    comentario

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,52 @@
 #!/urs/bin/python3
+
+"""[summary]
+
+    Raises:
+        TypeError: [description]
+        ValueError: [description]
+        TypeError: [description]
+        ValueError: [description]
+
+    Returns:
+        [type] -- [description]
+    """
+
+
 class Rectangle:
+    """[summary]
+    """
 
     def __init__(self, width=0, height=0):
+        """[summary]
+
+        Keyword Arguments:
+            width {int} -- [description] (default: {0})
+            height {int} -- [description] (default: {0})
+        """
         self.__height = height
         self.__width = width
 
     @property
     def height(self):
+        """[summary]
+
+        Returns:
+            [type] -- [description]
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """[summary]
+
+        Arguments:
+            value {[type]} -- [description]
+
+        Raises:
+            TypeError: [description]
+            ValueError: [description]
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -19,10 +55,24 @@ class Rectangle:
 
     @property
     def width(self):
+        """[summary]
+
+        Returns:
+            [type] -- [description]
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """[summary]
+
+        Arguments:
+            value {[type]} -- [description]
+
+        Raises:
+            TypeError: [description]
+            ValueError: [description]
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
