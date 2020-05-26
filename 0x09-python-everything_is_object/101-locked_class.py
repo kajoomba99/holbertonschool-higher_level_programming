5c5a671797e6c579b0c20872b632a74219357ee7
kajoomba99 <steven.brand1999gmail.com>
2020-05-26 18:40:00 -0500

    Updates

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""module for lock excercise"""
 
 
 class LockedClass:
-    __slots__ = ["first_name"]
+    """Class that prevents differente instance"""
+    __slots__ = 'first_name'
