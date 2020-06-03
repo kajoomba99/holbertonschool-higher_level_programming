6441228706d1f50d66338db32e04fad9ad691ac2
kajoomba99 <steven.brand1999gmail.com>
2020-06-02 19:56:37 -0500

    task# 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+MyInt Module
+"""
+
+
+class MyInt(int):
+    """
+    Subclass of int
+    """
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
