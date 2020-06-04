ec18686dc1d810d6f9b30fa98ae33ee7c80f8023
kajoomba99 <steven.brand1999gmail.com>
2020-06-03 23:45:52 -0500

    MMIB

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,10 +1,10 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """Module"""
 
 
 def append_write(filename="", text=""):
     """Function"""
     ln = 0
-    with open(filename, "a", encoding="utf-8") as a_file:
+    with open(filename, mode="a", encoding="utf-8") as a_file:
         ln = a_file.write(text)
     return ln
