4506e1f3f15c104cf115bb1fd8229b06b4ef7639
kajoomba99 <steven.brand1999gmail.com>
2020-06-03 23:35:41 -0500

    Style corrections

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Module"""
 
+
 def read_lines(filename="", nb_lines=0):
     """Function"""
-    number_of_lines = 0
-    with open(filename, encoding="utf-8") as a_file:
-        for _ in a_file:
-            number_of_lines += 1
 
+    number_of_lines = len(open(filename).readlines())
     with open(filename, encoding="utf-8") as a_file:
         if nb_lines > 0 and nb_lines < number_of_lines:
             for _ in range(nb_lines):
