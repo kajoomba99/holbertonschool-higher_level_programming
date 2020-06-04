0341ec85205e585f6b488d56953f0acc55843ca8
kajoomba99 <steven.brand1999gmail.com>
2020-06-04 13:04:08 -0500

    append_after function

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that contains append_after function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Function"""
+    with open(filename, encoding="utf-8") as a_file:
+        lines = a_file.readlines()
+        for line in lines:
+            if search_string in line:
+                lines.insert(lines.index(line) + 1, new_string)
+    new_line = "".join(lines)
+
+    with open(filename, mode="w", encoding="utf-8") as a_file:
+        a_file.write(new_line)
