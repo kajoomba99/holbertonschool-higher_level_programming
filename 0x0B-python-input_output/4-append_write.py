dac4bf331bb88788168d9b85e8ab9cbfa5ea18dd
kajoomba99 <steven.brand1999gmail.com>
2020-06-03 23:45:06 -0500

    correcting no sense stuff

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -5,6 +5,6 @@
 def append_write(filename="", text=""):
     """Function"""
     ln = 0
-    with open(filename, mode="a", encoding="utf-8") as a_file:
+    with open(filename, "a", encoding="utf-8") as a_file:
         ln = a_file.write(text)
     return ln
