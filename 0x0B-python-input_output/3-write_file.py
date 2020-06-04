de9fae8991d07717bb450d03f17794f5e8514a6e
kajoomba99 <steven.brand1999gmail.com>
2020-06-03 23:43:10 -0500

    Adding comments

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module"""
 
 
 def write_file(filename="", text=""):
+    """Function"""
     nl = 0
     with open(filename, mode="w", encoding="utf-8") as a_file:
         nl = a_file.write(text)
