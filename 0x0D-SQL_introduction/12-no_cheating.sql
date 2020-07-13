a6ba2b342e407b6771e61b5f8679c2682bcf80f3
Steve Brand <steven.brand1999@gmail.com>
2020-07-13 15:40:41 -0500

    updates

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,5 @@
 -- updates the score of Bob to 10
-UPDATE `second_table` SET `score` = 10 where `second_table`.`name` = 'Bob';
+UPDATE `second_table`
+SET
+`score` = 10
+where `second_table`.`name` = 'Bob';
