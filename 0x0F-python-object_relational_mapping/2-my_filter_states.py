234bf9201c5ef258c354cb743ff126521028408c
Steven Brand <steven.brand1999@gmail.com>
2020-08-09 12:26:00 -0500

    updates

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
             db=argv[3]
     )
     cur = db.cursor()
-    query = """SELECT * FROM states
-WHERE name = '{}'
-ORDER BY id ASC""".format(argv[4])
-    cur.execute(query)
+    cur.execute(
+        """SELECT * FROM states
+        WHERE name = '{}'
+        ORDER BY id ASC""".format(argv[4])
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
