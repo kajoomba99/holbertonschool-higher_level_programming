45822193f05fee3c76fa9fa60d4fbf24d57cf6a9
Steven Brand <steven.brand1999@gmail.com>
2020-08-24 17:18:25 -0500

    Style

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,14 @@ import urllib.parse
 from sys import argv
 
 
-url = argv[1]
-email = argv[2]
-value = {'email': email}
-data = urllib.parse.urlencode(value)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    value = {'email': email}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
-    print(the_page.decode('utf8'))
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf8'))
