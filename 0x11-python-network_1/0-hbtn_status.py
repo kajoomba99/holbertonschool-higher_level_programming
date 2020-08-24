5999d79c87454c69c269b5b5c618150193395aa9
Steven Brand <steven.brand1999@gmail.com>
2020-08-24 17:04:44 -0500

    Updates

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,13 @@
 import urllib.request
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as my_req:
-    respuesta = my_req.read()
-    print("""Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(
-         type(respuesta),
-         respuesta,
-         respuesta.decode('utf8')))
+if __name__ == "__main__":
+     with urllib.request.urlopen('https://intranet.hbtn.io/status') as my_req:
+          respuesta = my_req.read()
+          print("""Body response:
+          - type: {}
+          - content: {}
+          - utf8 content: {}""".format(
+               type(respuesta),
+               respuesta,
+               respuesta.decode('utf8')))
