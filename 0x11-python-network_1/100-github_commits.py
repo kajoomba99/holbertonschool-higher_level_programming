a391fd9ba6169697f4a905f359064e073db3c110
Steven Brand <steven.brand1999@gmail.com>
2020-08-24 19:08:12 -0500

    advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Some commentary"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    repo = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        user,
+        repo
+    )
+    req = requests.get(url).json()
+    for i in range(10):
+        sha = req[i].get('sha')
+        author_name = req[i].get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author_name))
