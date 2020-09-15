92e74371e7379f0936e50a321c8eaf0646af5384
steven brand <steven.brand1999@gmail.com>
2020-09-15 14:03:02 -0500

    style

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-const numberOfItems = strings.length
+const numberOfItems = strings.length;
 for (let i = 0; i < numberOfItems; i++) {
-  console.log(strings[i])
+  console.log(strings[i]);
 }
