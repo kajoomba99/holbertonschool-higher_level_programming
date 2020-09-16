e291a2cd8036759fc0457d09892fe05cb000f348
steven brand <steven.brand1999@gmail.com>
2020-09-15 21:17:51 -0500

    UPDATE

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
 if (!args[2] || isNaN(args[2])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   const x = parseInt(args[2]);
   for (let i = 0; i < x; i++) {
