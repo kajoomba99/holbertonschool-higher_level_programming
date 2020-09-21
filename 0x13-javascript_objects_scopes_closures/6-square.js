3a06d659f9fbdfe3b1ffb5bd0f4670207c5aef07
steven brand <steven.brand1999@gmail.com>
2020-09-21 17:03:54 -0500

    updates

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,14 +2,9 @@
 const BaseSquare = require('./5-square');
 
 module.exports = class Square extends BaseSquare {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
-
   charPrint (character = 'X') {
-    for (let i = 0; i < this.size; i++) {
-      console.log(character.repeat(this.size));
+    for (let i = 0; i < this.height; i++) {
+      console.log(character.repeat(this.width));
     }
   }
 };
