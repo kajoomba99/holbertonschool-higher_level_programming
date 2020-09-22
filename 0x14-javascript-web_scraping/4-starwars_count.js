8ac6ccf6056cbaa4394b95669049e2b251eb5a77
steven brand <steven.brand1999@gmail.com>
2020-09-22 13:44:27 -0500

    updates

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const args = process.argv;
-const url = args[2];
-request(url, (err, resp, body) => {
-  if (err) return console.log(err);
-  let wedge = 0;
-  const results = JSON.parse(body).results;
-  for (let i = 0; i < results.length; i++) {
-    const item = results[i];
-    const characters = item.characters;
-    if (characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      wedge += 1;
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    throw error;
+  }
+  const derulo = JSON.parse(body);
+  let c = 0;
+  for (const film of derulo.results) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        c++;
+      }
     }
   }
-  console.log(wedge);
+  console.log(c);
 });
