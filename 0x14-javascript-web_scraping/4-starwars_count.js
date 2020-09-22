acf4feb9c48fb6cda6506fed06f92951040f5a65
steven brand <steven.brand1999@gmail.com>
2020-09-22 13:40:13 -0500

    Updates

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-let wedge = 0;
 const args = process.argv;
 const url = args[2];
 request(url, (err, resp, body) => {
   if (err) return console.log(err);
-  const jsonResp = JSON.parse(body);
-  const { results } = jsonResp;
+  let wedge = 0;
+  const results = JSON.parse(body).results;
   for (let i = 0; i < results.length; i++) {
     const item = results[i];
     const characters = item.characters;
